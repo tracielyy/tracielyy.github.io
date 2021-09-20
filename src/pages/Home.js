f4d1f2ced39e372b3e108177f4dd3c69df1207d1
Tracie Ling Yan Ying <52907352+tracielyy@users.noreply.github.com>
2021-09-21 00:49:45 +0800

    remove counter & add useful info in about section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,61 +1,59 @@
 import { Fragment } from "react";
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // Import Components
-import Container from "../components/fragment/FragContainer.js"
+import Container from "../components/fragment/FragContainer.js";
 
 // Import Resources
-import { pageBgColor, fragBgColor, titleColor } from "../components/color/Color.js"
-
+import {
+  pageBgColor,
+  fragBgColor,
+  titleColor,
+} from "../components/color/Color.js";
 
 const Example = () => {
-    // Declare a new state variable, which we'll call "count"
-    const [count, setCount] = useState(0);
+  // Declare a new state variable, which we'll call "count"
+  const [count, setCount] = useState(0);
 
-    return (
-        <div>
-            <p>You clicked {count} times</p>
-            <button onClick={() => setCount(count + 1)}>
-                Click me
-            </button>
-        </div>
-    );
-}
-
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>Click me</button>
+    </div>
+  );
+};
 
 const HomeTitle = () => {
-    return (
-        <div>
-            <h1 style={{ 'color': titleColor, 'font-family': 'monospace' }}>
-                Welcome to Tracie Page
-            </h1>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1 style={{ color: titleColor, "font-family": "monospace" }}>
+        {"<Welcome /> ..."}
+      </h1>
+    </div>
+  );
+};
 
 // Home Body Contents
 const HomeContents = () => {
-    return (
-        <div>
-            <p>
-                This is some contentwecrwerwvrwetrebtwerwtwtvwertertretsssservteswervwerwer
-                erwetrwertveterte rtvertver
-                <Example />
-            </p>
-        </div>
-    );
-}
-
+  return (
+    <div>
+      <section style={{ "margin-top": "20px", "text-align": "left" }}>
+        <h4>About Me</h4>
+        <p>Hi guys, my name is Ling Yan Ying.</p>
+      </section>
+    </div>
+  );
+};
 
 const Home = () => {
-    return (
-        <Fragment>
-            <Container>
-                <HomeTitle />
-                <HomeContents />
-            </Container>
-        </Fragment>
-    );
-}
+  return (
+    <Fragment>
+      <Container>
+        <HomeTitle />
+        <HomeContents />
+      </Container>
+    </Fragment>
+  );
+};
 
-export default Home
+export default Home;

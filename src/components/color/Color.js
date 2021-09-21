9795f1bc61ab9a2148c4e85d67a58f10b016b352
Tracie Ling Yan Ying <52907352+tracielyy@users.noreply.github.com>
2021-09-21 23:26:31 +0800

    add a new color

--- src/components/color/Color.js
+++ src/components/color/Color.js
@@ -6,3 +6,4 @@ export const contentColor = "#2D2323"; // Darker Grey
 export const subContentColor = "#5F5C5A"; // Greyish
 export const lineColor = "#DEDCDC"; // Grey
 export const colorAccent = "#897562"; // Brown Color Accent
+export const lightAccent = "#C5BEBA"; // Light grey

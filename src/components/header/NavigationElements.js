41564740d49faedd4c6d29785f7bc33b6c30235c
Tracie Ling Yan Ying <52907352+tracielyy@users.noreply.github.com>
2021-09-21 22:39:31 +0800

    font change

--- src/components/header/NavigationElements.js
+++ src/components/header/NavigationElements.js
@@ -4,7 +4,13 @@ import styled from "styled-components";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // Import Resources
-import {pageBgColor, fragBgColor, titleColor, lineColor} from "../../components/color/Color.js"
+import {
+  pageBgColor,
+  fragBgColor,
+  contentColor,
+  titleColor,
+  lineColor,
+} from "../../components/color/Color.js";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
@@ -13,67 +19,52 @@ const media_max_width = "800px";
 const nav_height = "38px";
 
 // Hold Icon
-export const IconContainer = styled(Link)`
-
-
-`;
-
+export const IconContainer = styled(Link)``;
 
 // Navigation Bar
 export const NavBar = styled(Navbar)`
-
-	background-color: ${titleColor};
-	padding-right: 10px;
-	padding-left: 10px;
-	
-
+  background-color: ${titleColor};
+  padding-right: 10px;
+  padding-left: 10px;
 `;
-
 
 // Navigation Links
 export const NavLink = styled(Link)`
-    
-	font-family:monospace;
-    text-align: left;
-
-
+  font-family: arial;
+  font-size: 15px;
+  text-align: left;
 `;
 
 // Horizontal Line Encasing The Navigation Bar
 export const NavLine = styled.hr`
-
-	background-color: ${pageBgColor};
-    border:none;height:4px;
-    margin-top:0px;
-    margin-bottom:0px;
+  background-color: ${pageBgColor};
+  border: none;
+  height: 4px;
+  margin-top: 0px;
+  margin-bottom: 0px;
 `;
 
 // Vertical Line Separating Each Navigation Link
 export const NavDivider = styled.div`
+  background-color: #edf4f6;
+  height: 20px;
+  margin: 7px 0px;
+  width: 2px;
 
-	background-color:#EDF4F6;
-    height:15px;
-    margin: 7px 0px;
-    width:2px;
-
-    @media only screen and (max-width: ${media_max_width}) {
-        display:none;
-    }
+  @media only screen and (max-width: ${media_max_width}) {
+    display: none;
+  }
 `;
 
 // Register/Login
 export const RegLoginBtn = styled.button`
+  background-color: transparent;
+  border: 1.5px solid ${fragBgColor};
+  border-radius: 4px;
+  float: right;
+  margin: 50px 30px 0 0;
+  padding: 10px;
 
-	background-color:transparent;
-	border:1.5px solid ${fragBgColor};
-    border-radius: 4px;
-    float:right;
-    margin:50px 30px 0 0;
-    padding:10px;
-
-    @media only screen and (max-width: ${media_max_width}) {
-        
-    }
-
-
+  @media only screen and (max-width: ${media_max_width}) {
+  }
 `;

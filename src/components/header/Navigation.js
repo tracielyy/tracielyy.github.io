476a44a6ab42969fb1055dc7a6cd8f77c32e3550
Tracie Ling Yan Ying <52907352+tracielyy@users.noreply.github.com>
2021-09-21 22:40:12 +0800

    add icon and align the links to the right

--- src/components/header/Navigation.js
+++ src/components/header/Navigation.js
@@ -6,6 +6,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 // Import Pages
 import Home from "../../pages/Home.js";
 import Resume from "../../pages/Resume.js";
+import Projects from "../../pages/Projects.js";
 
 // Import Resources
 // Sequence of the import in {} is very important
@@ -22,7 +23,12 @@ import {
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
 import NavDropdown from "react-bootstrap/NavDropdown";
-import {pageBgColor, fragBgColor, titleColor, lineColor} from "../../components/color/Color.js"
+import {
+  pageBgColor,
+  fragBgColor,
+  titleColor,
+  lineColor,
+} from "../../components/color/Color.js";
 
 // Navigation Component
 const Navigation = () => {
@@ -30,23 +36,42 @@ const Navigation = () => {
     <Router>
       <NavBar collapseOnSelect expand="lg" variant="dark" sticky="top">
         <Navbar.Brand href={`${process.env.PUBLIC_URL}/`}>
-          Tracie Ling
+          <img src="android-chrome-512x512.png" height="40" width="40"/>
+          <span style={{"margin": "0px 10px", "font-family": "Lucida Console"}}>Tracie</span>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
+
+          <Nav
+            className="me-auto"
+            style={{ "justify-content": "end", width: "100%" }}
+          >
             {/* Resume */}
-            <NavLink id="active" to={`${process.env.PUBLIC_URL}/resume`} className="nav-link">
+            <NavLink
+              id="active"
+              as={Link}
+              to={`${process.env.PUBLIC_URL}/resume`}
+              className="nav-link"
+            >
               Resume
             </NavLink>
             {/* Project */}
-            <NavLink  to={`${process.env.PUBLIC_URL}/`} className="nav-link">Project</NavLink>
+            <NavLink
+              to={`${process.env.PUBLIC_URL}/projects`}
+              className="nav-link"
+            >
+              Project
+            </NavLink>
           </Nav>
         </Navbar.Collapse>
       </NavBar>
       <Switch>
         <Route path={`${process.env.PUBLIC_URL}/`} exact component={Home} />
         <Route path={`${process.env.PUBLIC_URL}/resume`} component={Resume} />
+        <Route
+          path={`${process.env.PUBLIC_URL}/projects`}
+          component={Projects}
+        />
       </Switch>
     </Router>
   );
